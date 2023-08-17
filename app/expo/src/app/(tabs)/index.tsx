18cf0e32704964d247e2e0431f080881b5f8e2c9
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2023-08-18 00:15:58 +0800

    test 2

--- app/expo/src/app/(tabs)/index.tsx
+++ app/expo/src/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Myy Tripss</Text>
+      <Text style={styles.title}>My Trips</Text>
       <View style={styles.separator} />
     </View>
   );
