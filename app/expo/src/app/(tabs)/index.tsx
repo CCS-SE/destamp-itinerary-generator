c920018481f8b053b84faf127708d29f40e227a0
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2023-08-17 23:48:21 +0800

    test

--- app/expo/src/app/(tabs)/index.tsx
+++ app/expo/src/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>My Trips</Text>
-      <View style={styles.separator}/>
+      <View style={styles.separator} />
     </View>
   );
 }
@@ -12,16 +12,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- app/expo/src/app/(tabs)/profile.tsx
+++ app/expo/src/app/(tabs)/profile.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profile</Text>
-      <View style={styles.separator}/>
+      <View style={styles.separator} />
     </View>
   );
 }
@@ -12,16 +12,16 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
