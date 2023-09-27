82ea7fb72f7322bf6a98f0d30d2bf8ce4cfe6569
Shem Jondanero <shemjehrojondanero@gmail.com>
2023-09-27 12:52:37 +0800

    logout unit test

---
+++ app/expo/src/components/Menu/ProfileMenu/__tests__/Logout.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import { act } from 'react-test-renderer';
+
+import { mockSupabaseClient } from '~/components/Forms/__mock__/mockSupabaseClient';
+import ProfileMenuList from '../ProfileMenuList';
+
+jest.mock('@supabase/supabase-js', () => {
+  return {
+    createClient: jest.fn(() => mockSupabaseClient),
+    error: null,
+  };
+});
+
+describe('Logout', () => {
+  it('renders the logout button', () => {
+    const { getByTestId } = render(<ProfileMenuList />);
+    const logoutBtn = getByTestId('Logout-btn');
+
+    expect(logoutBtn).toBeTruthy();
+  });
+
+  it('logs out a user', async () => {
+    const { getByTestId } = render(<ProfileMenuList />);
+    const logoutBtn = getByTestId('Logout-btn');
+
+    await act(() => {
+      fireEvent.press(logoutBtn);
+    });
+
+    expect(mockSupabaseClient.auth.signOut).toHaveBeenCalled();
+  });
+});
