391e9e858e04dff41e8a3a61bb0941f4e53ea307
karmslajo <karms.lajo@gmail.com>
2023-10-12 04:58:52 +0800

    add update expense resolver

--- packages/db/src/graphql/schema/mutation/Expense/Expense.resolver.ts
+++ packages/db/src/graphql/schema/mutation/Expense/Expense.resolver.ts
@@ -4,6 +4,7 @@ import { Context } from '../../../context';
 import { NexusGenInputs } from '../../../generated/nexus';
 
 type CreateExpenseInput = NexusGenInputs['CreateExpenseInput'];
+type UpdateExpenseInput = NexusGenInputs['UpdateExpenseInput'];
 
 export const createExpense = (args: CreateExpenseInput, ctx: Context) => {
   return ctx.prisma.expense.create({
@@ -21,6 +22,24 @@ export const deleteExpense = async (id: number, ctx: Context) => {
   return await ctx.prisma.expense.delete({
     where: {
       id: id,
+    },
+  });
+};
+
+export const updateExpense = async (
+  id: number,
+  input: UpdateExpenseInput,
+  ctx: Context,
+) => {
+  return await ctx.prisma.expense.update({
+    where: {
+      id: id,
+    },
+    data: {
+      amount: input.amount as number,
+      category: input.category as ExpenseCategory,
+      date: input.date as Date,
+      note: input.note as string,
     },
   });
 };
--- packages/db/src/graphql/schema/mutation/Expense/index.ts
+++ packages/db/src/graphql/schema/mutation/Expense/index.ts
@@ -1,5 +1,9 @@
-import { CreateExpense, DeleteExpense } from './Expense.mutation';
+import {
+  CreateExpense,
+  DeleteExpense,
+  UpdateExpense,
+} from './Expense.mutation';
 
-const Mutations = [CreateExpense, DeleteExpense];
+const Mutations = [CreateExpense, DeleteExpense, UpdateExpense];
 
 export default Mutations;
