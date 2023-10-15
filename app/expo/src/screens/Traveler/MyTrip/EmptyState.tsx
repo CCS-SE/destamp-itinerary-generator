ab1f42a05a89dd14a5382d36d5525c50f9a3cd0d
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2023-10-15 19:14:00 +0800

    Displayed user's name at EmptyState

--- app/expo/src/screens/Traveler/MyTrip/EmptyState.tsx
+++ app/expo/src/screens/Traveler/MyTrip/EmptyState.tsx
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import * as Haptics from 'expo-haptics';
 import { Image } from 'expo-image';
 import { LinearGradient } from 'expo-linear-gradient';
 import { router } from 'expo-router';
+import { useQuery } from '@apollo/client';
+
+import { AuthContext } from '~/context/AuthProvider';
+import { GetTravelerInfoDocument } from '~/graphql/generated';
 
 export default function MyTripEmptyState() {
   const onPress = () => {
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     router.push('/trip/create/');
   };
+  const { session } = useContext(AuthContext);
+  const { data, error } = useQuery(GetTravelerInfoDocument, {
+    variables: {
+      userId: session ? session.user.id : '',
+    },
+  });
+
+  if (error)
+    return (
+      <Text testID="my-trip-error">{`Error! ${error.message.toString()}`}</Text>
+    );
+
   return (
     <View testID="my-trip-empty-state" className="flex-1 items-center bg-white">
       <Image
         source={require('../../../../assets/images/empty-state.png')}
-        className="m-14 h-64 w-80"
+        className="m-14 h-60 w-80"
       />
+      <View className="text-center ">
+        <Text className="font-poppins text-xl font-normal text-slate-700">
+          Welcome, {data?.traveler.firstName}!
+        </Text>
+      </View>
+
       <Text
         testID="empty-state-title"
-        className="-my-3 font-poppins text-2xl font-normal text-slate-700"
+        className="pt-3 font-poppins text-lg font-normal text-slate-600"
       >
-        No trips yet
+        You have no trips yet
       </Text>
       <Text
         testID="empty-state-subtitle"
-        className="my-5 font-poppins text-lg font-normal text-slate-500"
+        className="px-2 pb-5 font-poppins text-base font-normal text-slate-500"
       >
         Start planning your adventure!
       </Text>
