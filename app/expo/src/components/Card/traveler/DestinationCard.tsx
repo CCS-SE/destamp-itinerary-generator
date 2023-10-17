e7b5e2aff58bade040c46b109e7c280187ccf5f4
karmslajo <karms.lajo@gmail.com>
2023-10-17 18:04:34 +0800

    fix format erorr

--- app/expo/src/components/Card/traveler/DestinationCard.tsx
+++ app/expo/src/components/Card/traveler/DestinationCard.tsx
@@ -108,7 +108,7 @@ export default function DestinationCard({
               className="justify-end"
               onPress={() => setAddExpenseModal(true)}
             >
-              <View className="justify-end mb-0.5 mr-3">
+              <View className="mb-0.5 mr-3 justify-end">
                 <MaterialCommunityIcons
                   name="cash-plus"
                   size={25}
