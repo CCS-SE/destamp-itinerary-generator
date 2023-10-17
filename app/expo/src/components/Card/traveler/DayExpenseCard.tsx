30bf036624ba239660d6843d0d1d7b216dbf4f1c
karmslajo <karms.lajo@gmail.com>
2023-10-17 08:10:41 +0800

    display accommodation cost

--- app/expo/src/components/Card/traveler/DayExpenseCard.tsx
+++ app/expo/src/components/Card/traveler/DayExpenseCard.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from 'react-native';
 
 import { amountFormatter } from '~/utils/utils';
+import Accommodation from '../../../../assets/images/accommodation.svg';
 import Attraction from '../../../../assets/images/attraction.svg';
 import Food from '../../../../assets/images/food.svg';
 import Transportation from '../../../../assets/images/transportation.svg';
 
 interface DayExpenseCardProps {
+  accommodationCost: number;
   attractionCost: number;
   foodCost: string;
   transportationCost: number;
@@ -17,6 +19,7 @@ interface DayExpenseTextProps {
 }
 
 export default function DayExpenseCard({
+  accommodationCost,
   attractionCost,
   foodCost,
   transportationCost,
@@ -27,24 +30,28 @@ export default function DayExpenseCard({
 
   return (
     <View className="mt-5 h-[75] w-[360] rounded-2xl bg-pink-100 p-2">
-      <View className="flex-row ">
+      <View className="flex-row">
         <Text className="m-1 font-poppins text-base text-gray-500">
           Day Expenses
         </Text>
-        <Text className="absolute left-[260] m-1  font-poppins-medium text-base text-[#F65A82]">
-          Total
-        </Text>
+        <View className="absolute left-[230] flex-row items-center ">
+          <Text className="m-1 font-poppins-medium text-base text-[#F65A82]">
+            Total:
+          </Text>
+          <Text className="font-poppins-medium text-base font-medium text-[#F65A82]">
+            {formatCurrency(totalCost)}
+          </Text>
+        </View>
       </View>
       <View className="flex-row items-center">
-        <Attraction height={18} width={18} style={{ marginLeft: 3 }} />
+        <Accommodation height={18} width={18} style={{ marginLeft: 3 }} />
+        <DayExpenseText value={formatCurrency(accommodationCost)} />
+        <Attraction height={18} width={18} />
         <DayExpenseText value={formatCurrency(attractionCost)} />
         <Food height={18} width={18} />
         <DayExpenseText value={foodCost} />
         <Transportation height={18} width={18} />
         <DayExpenseText value={formatCurrency(transportationCost)} />
-        <Text className="absolute left-[265] font-poppins-medium text-base font-medium text-[#F65A82]">
-          {formatCurrency(totalCost)}
-        </Text>
       </View>
     </View>
   );
@@ -52,6 +59,8 @@ export default function DayExpenseCard({
 
 const DayExpenseText = ({ value }: DayExpenseTextProps) => {
   return (
-    <Text className=" mx-2 font-poppins text-base text-gray-500 ">{value}</Text>
+    <Text className=" mx-1.5 font-poppins text-base text-gray-500 ">
+      {value}
+    </Text>
   );
 };
