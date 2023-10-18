f07809847c8b7e8f017451d14759dddd24a7f76c
Shem Jondanero <shemjehrojondanero@gmail.com>
2023-10-18 08:09:45 +0800

    fix overflowing UI

--- app/expo/src/components/Card/traveler/BudgetCategoryCard.tsx
+++ app/expo/src/components/Card/traveler/BudgetCategoryCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
 
 interface BudgetCategoryCardProps {
   icon: React.ReactNode;
@@ -15,7 +15,7 @@ const BudgetCategoryCard = ({
 }: BudgetCategoryCardProps) => {
   return isSelected ? (
     <TouchableOpacity onPress={onPress} activeOpacity={0.8}>
-      <View className="m-2 h-[75] w-[100] items-center justify-center rounded-lg bg-orange-100">
+      <View className="my-2 ml-2 h-[75] w-[100] items-center justify-center rounded-lg bg-orange-100">
         {icon}
         <Text className="font-poppins-medium text-[11px] text-orange-500">
           {title}
@@ -24,7 +24,7 @@ const BudgetCategoryCard = ({
     </TouchableOpacity>
   ) : (
     <TouchableOpacity onPress={onPress} activeOpacity={0.8}>
-      <View className="m-2 h-[75] w-[100] items-center justify-center rounded-lg bg-gray-100">
+      <View className="my-2 ml-2 h-[75] w-[100] items-center justify-center rounded-lg bg-gray-100">
         {icon}
         <Text className="font-poppins text-[11px] text-gray-500">{title}</Text>
       </View>
