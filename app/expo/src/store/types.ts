0f5a98ac5976e9ff40079f72ead26d23d1278a0e
karmslajo <karms.lajo@gmail.com>
2023-11-13 00:20:32 +0800

    integrate zustand for state management and add useFormStore

---
+++ app/expo/src/store/types.ts
@@ -0,0 +1,41 @@
+import { Moment } from 'moment';
+
+import { ExpenseCategory, TravelSize } from '~/graphql/generated';
+
+export type Activities = {
+  Sightseeing?: number;
+  Museum?: number;
+  Outdoor?: number;
+  Arts?: number;
+  Shopping?: number;
+  Landmarks?: number;
+};
+
+export interface CreateTripData {
+  destination: string;
+  startingLocation: {
+    place_name: string;
+    name: string;
+    center: [number, number];
+  };
+  travelSize: TravelSize;
+  startDate: Moment | null;
+  endDate: Moment | null;
+  timeslots: [number, number][];
+  budgetInclusions: [ExpenseCategory];
+  travelerCount: number;
+  budget: string;
+}
+
+export interface TripPreferenceData {
+  accommodationType: string;
+  amenities: string[];
+  activities: Activities;
+  diningStyles: string[];
+  cuisines: string[];
+  transportationStyle: string;
+}
+
+export interface ReviewTripData {
+  title: string;
+}
