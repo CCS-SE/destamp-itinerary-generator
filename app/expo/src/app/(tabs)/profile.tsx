0677aec911bf165ac782f72dbb07a5469d1dcd77
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2023-11-16 13:35:00 +0800

    Business Profile Screen and Verification Screens

--- app/expo/src/app/(tabs)/profile.tsx
+++ app/expo/src/app/(tabs)/profile.tsx
@@ -1,6 +1,5 @@
 import Profile from '~/screens/Traveler/Profile/Profile';
-import ChooseCategory from '../businessProfile/create/category';
 
 export default function ProfileScreen() {
-  return <ChooseCategory />;
+  return <Profile />;
 }
