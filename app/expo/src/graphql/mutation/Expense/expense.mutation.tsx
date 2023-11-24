2558f603f4b3077f6064d009ca3fc4f0785637db
karmslajo <karms.lajo@gmail.com>
2023-11-24 20:01:07 +0800

    refactor apollo client mutations

--- app/expo/src/graphql/mutation/Expense/expense.mutation.tsx
+++ app/expo/src/graphql/mutation/Expense/expense.mutation.tsx
@@ -1,28 +1,25 @@
 import { gql } from '@apollo/client';
 
 export const createExpense = gql(
-  `mutation CreateExpense($data: CreateExpenseInput!) {
-        createExpense(data: $data) {
-          amount,
-          category,
-          date,
-          note,
-        }
-      }`,
+  `mutation CreateExpense($tripId: Int!, $data: CreateExpenseInput!) {
+    createExpense(tripId: $tripId, data: $data) {
+      id
+    }
+  }`,
 );
 
 export const UpdateExpense = gql(
-  `mutation UpdateExpense($updateExpenseId: Int!, $data: UpdateExpenseInput!) {
-        updateExpense(id: $updateExpenseId, data: $data) {
-          id
-        }
-      }`,
+  `mutation UpdateExpense($expenseId: Int!, $data: UpdateExpenseInput!) {
+    updateExpense(id: $expenseId, data: $data) {
+      id
+    }
+  }`,
 );
 
 export const DeleteExpense = gql(
-  `mutation DeleteExpense($deleteExpenseId: Int!) {
-        deleteExpense(id: $deleteExpenseId) {
-          id
-        }
-      }`,
+  `mutation DeleteExpense($expenseId: Int!) {
+    deleteExpense(id: $expenseId) {
+      id
+    }
+  }`,
 );
