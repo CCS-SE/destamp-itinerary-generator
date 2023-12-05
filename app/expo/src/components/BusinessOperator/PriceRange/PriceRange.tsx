5d469832dd7918e180be006e700b11dc7e37f3d5
Chloe Belle ESTILO <chloebelle.estilo-20@cpu.edu.ph>
2023-12-06 04:07:56 +0800

    deleted same function components

--- app/expo/src/components/BusinessOperator/PriceRange/PriceRange.tsx
+++ app/expo/src/components/BusinessOperator/PriceRange/PriceRange.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import PriceRangeInput from '~/components/BusinessOperator/PriceRange/PriceRangeInput';
 import Questions from '~/components/BusinessOperator/Question';
 
-const RestaurantPriceRange = () => {
+const AccommodationPriceRange = () => {
   const [minPrice, setMinPrice] = useState<number>(0);
   const [maxPrice, setMaxPrice] = useState<number>(0);
 
@@ -22,29 +22,12 @@ const RestaurantPriceRange = () => {
     }
   };
 
-  // const handleNextButton = () => {
-  //   console.log('minPrice:', minPrice);
-  //   console.log('maxPrice:', maxPrice);
-  //   if (minPrice === 0 && maxPrice === 0) {
-  //     console.log('Invalid Price Range');
-  //     window.alert('Invalid Price Range: Please input a price range.');
-  //   } else if (minPrice >= maxPrice) {
-  //     console.log('Invalid Price Range');
-  //     window.alert(
-  //       'Invalid Price Range: Minimum price should be lower than the maximum price.',
-  //     );
-  //   } else {
-  //     console.log('Navigating to the next screen');
-  //     //   router.push('/business/create/establishmentType');
-  //   }
-  // };
-
   return (
     <View style={{ backgroundColor: 'white', flex: 1 }}>
       <SafeAreaView>
-        <Questions question={'Average price range per person'} />
-        <View style={{ flexDirection: 'row', margin: 10 }}></View>
-        <View style={{ alignItems: 'center' }}>
+        <Questions question={'Average price range of room per night'} />
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}></View>
+        <View style={{ alignItems: 'center', margin: 10 }}>
           <PriceRangeInput
             minPrice={minPrice}
             maxPrice={maxPrice}
@@ -56,4 +39,4 @@ const RestaurantPriceRange = () => {
     </View>
   );
 };
-export default RestaurantPriceRange;
+export default AccommodationPriceRange;
