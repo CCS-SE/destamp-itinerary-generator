848f33047edafb39a11338cda3a846662ff7b4fc
karmslajo <karms.lajo@gmail.com>
2023-12-11 00:46:45 +0800

    add initial loading screen

--- app/expo/src/app/[...missing].tsx
+++ app/expo/src/app/[...missing].tsx
@@ -1,38 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Link, Stack } from 'expo-router';
+import { ActivityIndicator, View } from 'react-native';
+import { Stack } from 'expo-router';
 
-export default function NotFoundScreen() {
+export default function LoadingScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
-
-        <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
-        </Link>
-      </View>
-    </>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
+      />
+      <ActivityIndicator size="large" color={'gray'} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
