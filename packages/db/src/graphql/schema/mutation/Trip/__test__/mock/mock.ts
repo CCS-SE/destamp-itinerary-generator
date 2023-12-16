ac7bb4825df10915a6b60fb843daa544832567cc
karmslajo <karms.lajo@gmail.com>
2023-12-17 04:18:12 +0800

    test: unit tests for mutation operations

---
+++ packages/db/src/graphql/schema/mutation/Trip/__test__/mock/mock.ts
@@ -0,0 +1,628 @@
+export const pointOfInterests = [
+  {
+    accommodation: {
+      id: 1,
+      amenities: [
+        {
+          name: 'Wi-Fi',
+        },
+        {
+          name: 'Air-conditioned',
+        },
+        {
+          name: 'Kid-friendly',
+        },
+        {
+          name: 'Restaurant',
+        },
+      ],
+    },
+    address: 'MHV7+P9P, Iloilo City Proper, Iloilo City, Iloilo, Philippines',
+    categories: [],
+    contactNumber: '+63333384288',
+    createdAt: new Date('2023-12-10T10:17:43.911Z'),
+    description: null,
+    id: 'clpzbzqp40000v7ngjy5p8yzm',
+    isAttraction: false,
+    latitude: 10.694334,
+    longitude: 122.5634381,
+    name: 'One Lourdes DORMITEL',
+    operatingHours: [],
+    price: '660',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:17:43.911Z'),
+    userId: null,
+    visitDuration: 1440,
+  },
+  {
+    accommodation: {
+      id: 2,
+      amenities: [
+        {
+          name: 'Wi-Fi',
+        },
+        {
+          name: 'Air-conditioned',
+        },
+        {
+          name: 'Kid-friendly',
+        },
+        {
+          name: 'Restaurant',
+        },
+      ],
+    },
+    address: 'General Luna St, La Paz, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 2,
+      },
+    ],
+    contactNumber: '+63333234277',
+    createdAt: new Date('2023-12-10T10:17:54.242Z'),
+    description: null,
+    id: 'clpzbzyo20006v7ng0xgtb3a8',
+    isAttraction: false,
+    latitude: 10.7097639,
+    longitude: 122.5664806,
+    name: 'Urban Inn Iloilo',
+    operatingHours: [],
+    price: '880',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:17:54.242Z'),
+    userId: null,
+    visitDuration: 1440,
+  },
+  {
+    accommodation: {
+      id: 3,
+      amenities: [
+        {
+          name: 'Wi-Fi',
+        },
+        {
+          name: 'Air-conditioned',
+        },
+        {
+          name: 'Kid-friendly',
+        },
+        {
+          name: 'Restaurant',
+        },
+      ],
+    },
+    address: 'Rizal St, Iloilo City Proper, Iloilo City, Iloilo, Philippines',
+    categories: [],
+    contactNumber: '+63333330000',
+    createdAt: new Date('2023-12-10T10:18:03.121Z'),
+    description: null,
+    id: 'clpzc05oz000cv7ngamfap488',
+    isAttraction: false,
+    latitude: 10.6921949,
+    longitude: 122.5709727,
+    name: 'Goldberry Lite Hotel Iloilo',
+    operatingHours: [],
+    price: '1045',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:18:03.121Z'),
+    userId: null,
+    visitDuration: 1440,
+  },
+  {
+    accommodation: {
+      id: 4,
+      amenities: [],
+    },
+    address:
+      'Benigno Aquino Ave, Mandurriao, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 3,
+      },
+    ],
+    contactNumber: '+63335090202',
+    createdAt: new Date('2023-12-10T10:18:12.749Z'),
+    description: null,
+    id: 'clpzc0cy5000iv7ng850rrplg',
+    isAttraction: false,
+    latitude: 10.70578,
+    longitude: 122.551114,
+    name: 'MO2 WESTOWN HOTEL',
+    operatingHours: [],
+    price: '1485',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:18:12.749Z'),
+    userId: null,
+    visitDuration: 1440,
+  },
+
+  {
+    accommodation: null,
+    address: 'Megaworld Blvd, Mandurriao, Iloilo City, Iloilo, Philippines',
+    categories: [
+      {
+        id: 5,
+      },
+    ],
+    contactNumber: '+639204781980',
+    createdAt: new Date('2023-12-10T10:36:49.724Z'),
+    description: '',
+    id: 'clpzcoat800w7v7nguia1lf54',
+    isAttraction: false,
+    latitude: 10.71795,
+    longitude: 122.5467471,
+    name: 'Butot Balat',
+    operatingHours: [],
+    price: '200-400',
+    restaurant: {
+      id: 128,
+      atmospheres: ['Casual'],
+    },
+    updatedAt: new Date('2023-12-10T10:36:49.724Z'),
+    userId: null,
+    visitDuration: 60,
+  },
+  {
+    accommodation: null,
+    address:
+      'MGJ7+4RF, Baluarte-Calumpang-Villa-Oton Boulevard, Barangay, Villa Arevalo District, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 21,
+      },
+      {
+        id: 24,
+      },
+    ],
+    contactNumber: '+63333377809',
+    createdAt: new Date('2023-12-10T10:36:54.185Z'),
+    description: '',
+    id: 'clpzcoeff00wdv7ngoflsxca6',
+    isAttraction: false,
+    latitude: 10.6803235,
+    longitude: 122.5145334,
+    name: 'Ernings Manokan & Seafood',
+    operatingHours: [
+      {
+        id: 840,
+      },
+      {
+        id: 834,
+      },
+      {
+        id: 835,
+      },
+      {
+        id: 836,
+      },
+      {
+        id: 837,
+      },
+      {
+        id: 838,
+      },
+      {
+        id: 839,
+      },
+    ],
+    price: '200-400',
+    restaurant: {
+      id: 129,
+      atmospheres: ['Casual'],
+    },
+    updatedAt: new Date('2023-12-10T10:36:54.185Z'),
+    userId: null,
+    visitDuration: 60,
+  },
+  {
+    accommodation: null,
+    address:
+      'Iloilo Interior Building, Benigno Aquino Ave, Mandurriao, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 55,
+      },
+      {
+        id: 57,
+      },
+    ],
+    contactNumber: '',
+    createdAt: new Date('2023-12-10T10:50:17.284Z'),
+    description: '',
+    id: 'clpzd5lxf01o6v7ng981rcpff',
+    isAttraction: true,
+    latitude: 10.7066865,
+    longitude: 122.5491016,
+    name: 'Atria Park District',
+    operatingHours: [
+      {
+        id: 1848,
+      },
+      {
+        id: 1842,
+      },
+      {
+        id: 1843,
+      },
+      {
+        id: 1844,
+      },
+      {
+        id: 1845,
+      },
+      {
+        id: 1846,
+      },
+      {
+        id: 1847,
+      },
+    ],
+    price: '0',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:50:17.284Z'),
+    userId: null,
+    visitDuration: 90,
+  },
+  {
+    accommodation: null,
+    address:
+      'Sofia Reyes de Veyra Monument, Yulo Dr, Villa Arevalo District, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 52,
+      },
+      {
+        id: 53,
+      },
+    ],
+    contactNumber: '+639103774994',
+    createdAt: new Date('2023-12-10T10:50:23.196Z'),
+    description: '',
+    id: 'clpzd5qny01ocv7ngd56k4ez9',
+    isAttraction: true,
+    latitude: 10.6886221,
+    longitude: 122.5158816,
+    name: 'Villa Arevalo District Plaza',
+    operatingHours: [
+      {
+        id: 1855,
+      },
+      {
+        id: 1849,
+      },
+      {
+        id: 1850,
+      },
+      {
+        id: 1851,
+      },
+      {
+        id: 1852,
+      },
+      {
+        id: 1853,
+      },
+      {
+        id: 1854,
+      },
+    ],
+    price: '0',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:50:23.196Z'),
+    userId: null,
+    visitDuration: 45,
+  },
+  {
+    accommodation: {
+      id: 10,
+      amenities: [
+        {
+          name: 'Wi-Fi',
+        },
+        {
+          name: 'Air-conditioned',
+        },
+        {
+          name: 'Kid-friendly',
+        },
+        {
+          name: 'Restaurant',
+        },
+      ],
+    },
+    address: 'MHV7+P9P, Iloilo City Proper, Iloilo City, Iloilo, Philippines',
+    categories: [],
+    contactNumber: '+63333384288',
+    createdAt: new Date('2023-12-10T10:17:43.911Z'),
+    description: null,
+    id: 'clpzbzqp40000v7ngjy5p8yzc',
+    isAttraction: false,
+    latitude: 10.694334,
+    longitude: 122.5634381,
+    name: 'One Lourdes DORMITEL',
+    operatingHours: [],
+    price: '660',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:17:43.911Z'),
+    userId: null,
+    visitDuration: 1440,
+  },
+  {
+    accommodation: {
+      id: 20,
+      amenities: [
+        {
+          name: 'Wi-Fi',
+        },
+        {
+          name: 'Air-conditioned',
+        },
+        {
+          name: 'Kid-friendly',
+        },
+        {
+          name: 'Restaurant',
+        },
+      ],
+    },
+    address: 'General Luna St, La Paz, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 2,
+      },
+    ],
+    contactNumber: '+63333234277',
+    createdAt: new Date('2023-12-10T10:17:54.242Z'),
+    description: null,
+    id: 'clpzbzy920006v7ng0xgtb3a8',
+    isAttraction: false,
+    latitude: 10.7097639,
+    longitude: 122.5664806,
+    name: 'Urban Inn Iloilo',
+    operatingHours: [],
+    price: '880',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:17:54.242Z'),
+    userId: null,
+    visitDuration: 1440,
+  },
+  {
+    accommodation: {
+      id: 30,
+      amenities: [
+        {
+          name: 'Wi-Fi',
+        },
+        {
+          name: 'Air-conditioned',
+        },
+        {
+          name: 'Kid-friendly',
+        },
+        {
+          name: 'Restaurant',
+        },
+      ],
+    },
+    address: 'Rizal St, Iloilo City Proper, Iloilo City, Iloilo, Philippines',
+    categories: [],
+    contactNumber: '+63333330000',
+    createdAt: new Date('2023-12-10T10:18:03.121Z'),
+    description: null,
+    id: 'clpzc05oz000cv7ngbmfap488',
+    isAttraction: false,
+    latitude: 10.6921949,
+    longitude: 122.5709727,
+    name: 'Goldberry Lite Hotel Iloilo',
+    operatingHours: [],
+    price: '1045',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:18:03.121Z'),
+    userId: null,
+    visitDuration: 1440,
+  },
+  {
+    accommodation: {
+      id: 40,
+      amenities: [],
+    },
+    address:
+      'Benigno Aquino Ave, Mandurriao, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 3,
+      },
+    ],
+    contactNumber: '+63335090202',
+    createdAt: new Date('2023-12-10T10:18:12.749Z'),
+    description: null,
+    id: 'clpzc0cy5000iv7ng850rrplc',
+    isAttraction: false,
+    latitude: 10.70578,
+    longitude: 122.551114,
+    name: 'MO2 WESTOWN HOTEL',
+    operatingHours: [],
+    price: '1485',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:18:12.749Z'),
+    userId: null,
+    visitDuration: 1440,
+  },
+
+  {
+    accommodation: null,
+    address: 'Megaworld Blvd, Mandurriao, Iloilo City, Iloilo, Philippines',
+    categories: [
+      {
+        id: 5,
+      },
+    ],
+    contactNumber: '+639204781980',
+    createdAt: new Date('2023-12-10T10:36:49.724Z'),
+    description: '',
+    id: 'clpzcoat800w9v7nguia1lf54',
+    isAttraction: false,
+    latitude: 10.71795,
+    longitude: 122.5467471,
+    name: 'Butot Balat',
+    operatingHours: [],
+    price: '200-400',
+    restaurant: {
+      id: 128,
+      atmospheres: ['Casual'],
+    },
+    updatedAt: new Date('2023-12-10T10:36:49.724Z'),
+    userId: null,
+    visitDuration: 60,
+  },
+  {
+    accommodation: null,
+    address:
+      'MGJ7+4RF, Baluarte-Calumpang-Villa-Oton Boulevard, Barangay, Villa Arevalo District, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 21,
+      },
+      {
+        id: 24,
+      },
+    ],
+    contactNumber: '+63333377809',
+    createdAt: new Date('2023-12-10T10:36:54.185Z'),
+    description: '',
+    id: 'clpzfoeff00wdv7ngoflsxca6',
+    isAttraction: false,
+    latitude: 10.6803235,
+    longitude: 122.5145334,
+    name: 'Ernings Manokan & Seafood',
+    operatingHours: [
+      {
+        id: 840,
+      },
+      {
+        id: 834,
+      },
+      {
+        id: 835,
+      },
+      {
+        id: 836,
+      },
+      {
+        id: 837,
+      },
+      {
+        id: 838,
+      },
+      {
+        id: 839,
+      },
+    ],
+    price: '200-400',
+    restaurant: {
+      id: 129,
+      atmospheres: ['Casual'],
+    },
+    updatedAt: new Date('2023-12-10T10:36:54.185Z'),
+    userId: null,
+    visitDuration: 60,
+  },
+  {
+    accommodation: null,
+    address:
+      'Iloilo Interior Building, Benigno Aquino Ave, Mandurriao, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 55,
+      },
+      {
+        id: 57,
+      },
+    ],
+    contactNumber: '',
+    createdAt: new Date('2023-12-10T10:50:17.284Z'),
+    description: '',
+    id: 'clpzd5lxf81o6v7ng981rcpff',
+    isAttraction: true,
+    latitude: 10.7066865,
+    longitude: 122.5491016,
+    name: 'Atria Park District',
+    operatingHours: [
+      {
+        id: 1848,
+      },
+      {
+        id: 1842,
+      },
+      {
+        id: 1843,
+      },
+      {
+        id: 1844,
+      },
+      {
+        id: 1845,
+      },
+      {
+        id: 1846,
+      },
+      {
+        id: 1847,
+      },
+    ],
+    price: '0',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:50:17.284Z'),
+    userId: null,
+    visitDuration: 90,
+  },
+  {
+    accommodation: null,
+    address:
+      'Sofia Reyes de Veyra Monument, Yulo Dr, Villa Arevalo District, Iloilo City, 5000 Iloilo, Philippines',
+    categories: [
+      {
+        id: 52,
+      },
+      {
+        id: 53,
+      },
+    ],
+    contactNumber: '+639103774994',
+    createdAt: new Date('2023-12-10T10:50:23.196Z'),
+    description: '',
+    id: 'clpzd5qmy01ocv7ngd56k4ez9',
+    isAttraction: true,
+    latitude: 10.6886221,
+    longitude: 122.5158816,
+    name: 'Villa Arevalo District Plaza',
+    operatingHours: [
+      {
+        id: 1855,
+      },
+      {
+        id: 1849,
+      },
+      {
+        id: 1850,
+      },
+      {
+        id: 1851,
+      },
+      {
+        id: 1852,
+      },
+      {
+        id: 1853,
+      },
+      {
+        id: 1854,
+      },
+    ],
+    price: '0',
+    restaurant: null,
+    updatedAt: new Date('2023-12-10T10:50:23.196Z'),
+    userId: null,
+    visitDuration: 45,
+  },
+];
